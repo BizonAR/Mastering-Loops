37b153cf02a43f25decd7be2e0302aad43ce8630
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 00:47:21 +0500

    Mastering Loops

---
+++ MasteringLoops.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MasteringLoops
+{
+	internal class MasteringLoops
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите сообщение: ");
+			string message = Console.ReadLine();
+			Console.Write("Введите количество повторений: ");
+			int numberRepitions = Convert.ToInt32(Console.ReadLine());
+
+			for (int i = 0; i < numberRepitions; ++i)
+				Console.WriteLine(message);
+		}
+	}
+}
